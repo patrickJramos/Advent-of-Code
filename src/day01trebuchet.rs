d43ee051256e185c3eee6fefc909eacac6b83a22
patrick.ramos <patrick.ramos@marvee.com.br>
2024-02-04 09:12:38 -0300

    refactor trebuchet

--- src/day01trebuchet.rs
+++ src/day01trebuchet.rs
@@ -1,44 +1,37 @@
 pub fn trebuchet_2(str: String) -> usize {
-    let split = str.split('\n');
-    let mut sum: usize = 0;
+    str.lines()
+        .map(|line| {
+            let mut first: Option<usize> = None;
+            let mut last: Option<usize> = None;
 
-    split.for_each(|line| {
-        let mut first: Option<usize> = None;
-        let mut last: Option<usize> = None;
+            let len = line.len();
+            let mut n = 0;
 
-        let len = line.len();
-        let mut n = 0;
+            while n < len {
+                let num: Option<usize> = line[n..n + 1].parse::<usize>().ok().or(NUM_STRS
+                    .iter()
+                    .enumerate()
+                    .find_map(|(i, &str)| {
+                        line[n..].starts_with(str).then(|| {
+                            // we skip to the last character of the string. we can't skip the whole string
+                            // because the end of a number might match with the start of another.
+                            // looking at the strings that might match, there are no endings that have
+                            // 2 characters in common with the start of another string, se we can go to len - 1.
+                            // we do len - 2 because at the end of the loop we += 1
+                            n += str.len() - 2;
+                            // nums in the array are sorted, starting at 0, so we add 1
+                            i + 1
+                        })
+                    }));
 
-        while n < len {
-            let num: Option<usize> = if let Some(digit) = line[n..n + 1].parse::<usize>().ok() {
-                Some(digit as usize)
-            } else {
-                NUM_STRS.iter().enumerate().find_map(|(i, &str)| {
-                    if line[n..].starts_with(str) {
-                        // we skip to the last character of the string. we can't skip the whole string
-                        // because the end of a number might match with the start of another.
-                        // looking at the string that might match, there are no endings that have
-                        // 2 characters in common with the start of another string, se we can go to len - 1
+                first = first.or(num);
+                last = num.or(last);
+                n += 1;
+            }
 
-                        n += str.len() - 2;
-                        Some(i + 1)
-                    } else {
-                        None
-                    }
-                })
-            };
-
-            first = first.or(num);
-            last = num.or(last);
-            n += 1;
-        }
-
-        let s = first.unwrap_or(0) * 10 + last.unwrap_or(0);
-
-        sum += s;
-    });
-
-    sum
+            first.expect("should always exist") * 10 + last.expect("should always exist")
+        })
+        .sum()
 }
 
 const NUM_STRS: [&str; 9] = [
